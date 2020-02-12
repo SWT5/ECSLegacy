e84bc3ea4c4c476f6148d34bf83739d6a59310b8
Nikolaj Huynh <201706357@post.au.dk>
2020-02-12 16:04:19 +0100

    Class temp made to public and project for test added with uut

---
+++ ECS.Legacy.Test.Unit/ECSLegacyTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ECS.Legacy.Test.Unit
+{
+    public class ECSLegacyTest
+    {
+        private ECS uut;
+        private Heater h1 = new Heater();
+        private TempSensor t1 = new TempSensor();
+
+        [SetUp]
+        public void setUpFunction()
+        {
+           uut = new ECS(30, new Heater(), new TempSensor()); 
+        }
+
+
+
+    }
+}
